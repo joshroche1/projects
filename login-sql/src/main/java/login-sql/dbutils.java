bdbf6ed2db0b231c902c0e5f014b189e8da588d9
joshroche1 <joshua.a.roche@gmail.com>
2020-06-11 00:36:29 +0200

    Create dbutils.java

---
+++ login-sql/src/main/java/login-sql/dbutils.java
@@ -0,0 +1,25 @@
+package login-sql;
+
+import java.SOMETHING;
+import java.io.IOException;
+
+import javax.SOME;
+import javax.OTHER;
+import javax.THING;
+import javax.servlet.ServletException;
+
+public class dbutils extends SOMETHING {
+  
+  public/private/protected void/returntype methodName(Type Variable) throws IOException, ServletException {
+  
+    do a flip;
+    
+    while(do a little dance) {
+      if (make a little love) {
+        get down tonight;
+      } else {
+        cry;
+      }
+    }
+  
+}
