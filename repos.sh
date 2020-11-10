5a1a2aa2d2b50a29a10b635c30450a8bede2a095
joshroche1 <joshua.a.roche@gmail.com>
2020-11-10 21:48:08 +0100

    Create repos.sh

---
+++ repos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir JavaEE
+cd JavaEE
+git clone https://github.com/joshroche1/tomee-build.git
+git clone https://github.com/joshroche1/EJB-Facelets.git
+git clone https://github.com/joshroche1/SQL-CRUD-Example.git
+git clone https://github.com/joshroche1/Java-Server-Faces-Template.git
+git clone https://github.com/joshroche1/Servlet-Template.git
+git clone https://github.com/joshroche1/Facelets-Template.git
