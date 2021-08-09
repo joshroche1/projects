3027f0725e18b5e5b55ccc8f5491c5a27717cede
joshroche1 <joshua.a.roche@gmail.com>
2021-08-09 17:02:12 +0200

    Update build.sh

--- tomee-build/build.sh
+++ tomee-build/build.sh
@@ -10,12 +10,9 @@ mv apache-tomee-webprofile-8.0.4/ tomee/
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
 iptables-save
-useradd tomee
-username=tomee
-PASSWORD=ApacheTomEE
+useradd -p $(openssl passwd -crypt ApacheTomEE) tomee
 mkdir /home/tomee
 chown -R tomee: /home/tomee
-echo "$PASSWORD" | passwd --stdin "tomee"
 cp tomee.service /etc/systemd/system/
 systemctl daemon-reload 
 systemctl tomee start 
