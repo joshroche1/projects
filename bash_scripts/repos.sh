d43a126886f8be6e4003cf73cb263e5f240894bf
joshroche1 <joshua.a.roche@gmail.com>
2021-08-09 14:15:16 +0200

    Update repos.sh

--- bash_scripts/repos.sh
+++ bash_scripts/repos.sh
@@ -2,8 +2,5 @@
 mkdir JavaEE
 cd JavaEE
 git clone https://github.com/joshroche1/tomee-build.git
-git clone https://github.com/joshroche1/EJB-Facelets.git
-git clone https://github.com/joshroche1/SQL-CRUD-Example.git
-git clone https://github.com/joshroche1/Java-Server-Faces-Template.git
-git clone https://github.com/joshroche1/Servlet-Template.git
-git clone https://github.com/joshroche1/Facelets-Template.git
+git clone https://github.com/joshroche1/Java-EE.git
+git clone https://github.com/joshroche1/cmi
