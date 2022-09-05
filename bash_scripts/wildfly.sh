c8b71a987414874e63bed06a6fc421879109eea5
joshroche1 <joshua.a.roche@gmail.com>
2022-09-05 12:55:01 +0200

    Update wildfly.sh

--- bash_scripts/wildfly.sh
+++ bash_scripts/wildfly.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+apt-get update; apt-get upgrade -y
+apt install default-jdk
+
 wget https://github.com/wildfly/wildfly/releases/download/26.1.1.Final/wildfly-26.1.1.Final.tar.gz
 tar xvzf wildfly-26.1.1.Final.tar.gz
 mv wildfly-26.1.1.Final wildfly
 cd wildfly/bin
 ./add-user.sh --user admin --password admin --silent --enable
+
 echo '
 [Unit]
 Description=Wildfly Application Server
@@ -16,6 +20,7 @@ ExecStart=/opt/wildfly/bin/standalone.sh
 
 [Install]
 WantedBy=multi-user.target' > wildfly.service
+
 cp wildfly.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start wildfly.service
