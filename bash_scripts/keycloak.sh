5c2c4deb233ac0c2f12f8bec0f0c1659252d26c4
joshroche1 <joshua.a.roche@gmail.com>
2022-09-09 15:18:24 +0200

    Update keycloak.sh

--- bash_scripts/keycloak.sh
+++ bash_scripts/keycloak.sh
@@ -4,6 +4,12 @@
 apt-get update; apt-get upgrade -y
 apt-get install default-jdk
 
+apt-get install postgresql
+sudo -u postgres createuser -l -P keycloak
+sudo -u postgres createdb -O keycloak keycloak
+sed -i '85i local keycloak  keycloak  md5' /etc/postgresql/14/main/pg_hba.conf
+systemctl restart postgresql
+
 wget https://github.com/keycloak/keycloak/releases/download/19.0.1/keycloak-19.0.1.tar.gz
 
 tar xvzf keycloak-19.0.1.tar.gz
