fd5d3e19a2f1a9d52f032606d8ef88efb1099972
joshroche1 <joshua.a.roche@gmail.com>
2022-10-12 17:51:37 +0200

    .

--- bash_scripts/keycloak.sh
+++ bash_scripts/keycloak.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-
-apt-get update; apt-get upgrade -y
-apt-get install default-jdk
-
-apt-get install postgresql
-sudo -u postgres createuser -l -P keycloak
-sudo -u postgres createdb -O keycloak keycloak
-sed -i '85i local keycloak  keycloak  md5' /etc/postgresql/14/main/pg_hba.conf
-systemctl restart postgresql
-
-wget https://github.com/keycloak/keycloak/releases/download/19.0.1/keycloak-19.0.1.tar.gz
-
-tar xvzf keycloak-19.0.1.tar.gz
-mv keycloak-19.0.1 keycloak
+cd /opt
+wget https://github.com/keycloak/keycloak/releases/download/19.0.3/keycloak-19.0.3.tar.gz
+wget https://github.com/keycloak/keycloak/releases/download/19.0.3/keycloak-saml-wildfly-adapter-19.0.3.tar.gz
+tar keycloak-19.0.3.tar.gz
+mv keycloak-19.0.3 keycloak
+adduser --disabled-password --shell /usr/sbin/nologin --gecos "" keycloak
+chown -R keycloak:keycloak /opt/keycloak
 cd keycloak/bin
-./kc.sh build --db=postgres
+
+echo '
+export KEYCLOAK_ADMIN=admin
+export KEYCLOAK_ADMIN_PASSWORD=admin
+' > ../.env
+
+# To run the server:
+sudo -u keycloak ./kc.sh start-dev &
+
+# If you want to use systemd to run the application:
+#
+touch keycloak.service
+echo '
+[Unit]
+Description=Keycloak Server
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=keycloak
+EnvironmentFile=/opt/keycloak/.env
+ExecStart=/opt/keycloak/bin/kc.sh start-dev
+Restart=always
+RestartSec=20
+
+[Install]
+WantedBy=multi-user.target' >> keycloak.service
+cp keycloak.service /etc/systemd/system
+#systemctl daemon-reload
+#systemctl start keycloak.service
+#systemctl enable keycloak.service
--- bash_scripts/wildfly.sh
+++ bash_scripts/wildfly.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-apt-get update; apt-get upgrade -y
-apt install default-jdk
-
+cd /opt
 wget https://github.com/wildfly/wildfly/releases/download/26.1.1.Final/wildfly-26.1.1.Final.tar.gz
 tar xvzf wildfly-26.1.1.Final.tar.gz
 mv wildfly-26.1.1.Final wildfly
+adduser --disabled-password --shell /usr/sbin/nologin --gecos "" wildfly
+chown -R wildfly:wildfly /opt/wildfly
 cd wildfly/bin
 ./add-user.sh --user admin --password admin --silent --enable
-adduser --disabled-password --shell /usr/sbin/nologin --gecos "" wildfly
 
+chown -R wildfly: /opt/smc
+
+# To run the server:
+sudo -u wildfly ./standalone &
+
+# If you want to use systemd to run the application:
+#
+touch wildfly.service
 echo '
 [Unit]
-Description=WildFly application server
-Wants=network-online.target 
+Description=Wildfly Application Server
+Wants=network-online.target
 After=network-online.target
 
 [Service]
@@ -24,10 +31,8 @@ Restart=always
 RestartSec=20
 
 [Install]
-WantedBy=multi-user.target
-' > wildfly.service
-
+WantedBy=multi-user.target' >> wildfly.service
 cp wildfly.service /etc/systemd/system
-systemctl daemon-reload
-systemctl start wildfly.service
-systemctl enable wildfly.service
+#systemctl daemon-reload
+#systemctl start wildfly.service
+#systemctl enable wildfly.service
