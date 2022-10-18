963e9a7131d69ed9cc83b17aa828302b1a3ec8d7
joshroche1 <joshua.a.roche@gmail.com>
2022-10-18 14:22:48 +0000

    .

--- bash_scripts/prometheus-node.sh
+++ bash_scripts/prometheus-node.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#
 cd /opt
 wget https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-arm64.tar.gz
 tar xvzf node_exporter-1.4.0.linux-amd64.tar.gz
 mv node_exporter-1.4.0.linux-amd64 node_exporter
 adduser --disabled-password --shell /usr/sbin/nologin --gecos "" prometheus
