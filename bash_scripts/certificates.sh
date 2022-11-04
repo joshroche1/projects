f94a8a4cfca4ccfaa2a30714306a65eb19dec86c
joshroche1 <joshua.a.roche@gmail.com>
2022-11-04 14:14:09 +0100

    Update certificates.sh

--- bash_scripts/certificates.sh
+++ bash_scripts/certificates.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ssh-keygen -t rsa -b 4096
+CERT_SUBJ="/C=/ST=/L=/O=/OU=/CN=/"
+# "/C=DE/ST=NRW/L=Berlin/O=My Inc/OU=DevOps/CN=jar.net/emailAddress=admin@jar.net
 
 openssl \
     req \
@@ -8,7 +9,7 @@ openssl \
     -newkey rsa:4096 \
     -keyout server.key \
     -out server.csr \
-    -subj "/C=DE/ST=NRW/L=Berlin/O=My Inc/OU=DevOps/CN=jar.net/emailAddress=admin@jar.net"
+    -subj $CERT_SUBJ
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 cp server.crt /etc/ssl/certs
 cp server.key /etc/ssl/private
