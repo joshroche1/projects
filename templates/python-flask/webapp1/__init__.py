b4306b68567c9c3d51f7001ab79efc0a88215a54
joshroche1 <joshua.a.roche@gmail.com>
2022-12-14 12:11:01 +0100

    Update __init__.py

--- templates/python-flask/webapp1/__init__.py
+++ templates/python-flask/webapp1/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
   app = Flask(__name__, instance_relative_config=True)
   app.config.from_mapping(
     SECRET_KEY="dev",
-    DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    #DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
   )
   if test_config is None:
     app.config.from_pyfile("config.py", silent=True)
@@ -34,6 +34,9 @@ def create_app(test_config=None):
 
   from webapp1 import views
   app.register_blueprint(views.bp)
+  
+  from webapp1 import auth
+  app.register_blueprint(auth.bp)
 
   app.add_url_rule("/", endpoint="views.index")
 
