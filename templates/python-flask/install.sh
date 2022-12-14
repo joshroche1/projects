f5a49fa470b5ee1139cddab84490f3e5ed6733c4
joshroche1 <joshua.a.roche@gmail.com>
2022-12-14 14:39:11 +0100

    Update install.sh

--- templates/python-flask/install.sh
+++ templates/python-flask/install.sh
@@ -7,10 +7,12 @@ apt install -y python3 python3-pip \
         python3-wtforms python3-dotenv python3-psycopg2
 pip3 install flask-mobility
 #
-# OTHER DB OPTIONS
-#apt install -y postgresql
+# POSTGRESQL
+apt install -y postgresql python3-psycopg2
+
 #
 sudo -u postgres createuser -l -P webapp
 sudo -u postgres createdb -O webapp webapp
-echo 'local   webapp          webapp                                  scram-sha-256' >> /etc/postgresql/15/pg_hba.conf
+sed -i '80 i local   webapp          webapp                                  scram-sha-256' /etc/postgresql/15/main/pg_hba.conf
 systemctl restart postgresql
+#
