bf5f6bfceb203319b21ea885b79ce0c85622f68c
joshroche1 <joshua.a.roche@gmail.com>
2023-01-20 13:31:51 +0100

    Update

--- bash_scripts/prometheus-node.sh
+++ bash_scripts/prometheus-node.sh
@@ -2,13 +2,13 @@
 
 #
 cd /opt
-wget https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
 wget https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-arm64.tar.gz
 wget https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-armv7.tar.gz
 wget https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-armv6.tar.gz
 tar xvzf node_exporter-1.4.0.linux-amd64.tar.gz
 mv node_exporter-1.4.0.linux-amd64 node_exporter
-adduser --disabled-password --shell /usr/sbin/nologin --gecos "" prometheus
+adduser --system --disabled-password --shell /usr/sbin/nologin --gecos "" prometheus
 
 echo '
 [Unit]
