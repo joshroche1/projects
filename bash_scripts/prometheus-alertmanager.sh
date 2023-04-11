241c4736023189cd0b0c46ccb586b849f2287265
joshroche1 <joshua.a.roche@gmail.com>
2023-04-11 10:09:29 +0200

    Create prometheus-alertmanager.sh

---
+++ bash_scripts/prometheus-alertmanager.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+cd /opt
+wget https://github.com/prometheus/alertmanager/releases/download/v0.25.0/alertmanager-0.25.0.linux-amd64.tar.gz
+mkdir alertmanager
+tar xvzf alertmanager-0.25.0.linux-amd64.tar.gz
+mv alertmanager-0.25.0* alertmanager
+adduser --system --disabled-password --shell /usr/sbin/nologin --gecos "" prometheus
+chown -R prometheus: /opt/alertmanager/
+echo '
+[Unit]
+Description=Prometheus - Alert Manager
+After=network.target
+
+[Service]
+User=prometheus
+Type=simple
+ExecStart=/opt/alertmanager/alertmanager
+
+[Install]
+WantedBy=multi-user.target' > alertmanager.service
+# cp alertmanager.service /etc/systemd/system/
+# systemctl daemon-reload
+# systemctl enable node_exporter.service
+# systemctl start node_exporter.service
