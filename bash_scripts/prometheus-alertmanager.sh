6e23986f57ffb245d1dcb129894a98f594bf8e11
joshroche1 <joshua.a.roche@gmail.com>
2023-05-02 20:07:27 +0200

    Update prometheus-alertmanager.sh

--- bash_scripts/prometheus-alertmanager.sh
+++ bash_scripts/prometheus-alertmanager.sh
@@ -3,10 +3,10 @@
 #
 cd /opt
 wget https://github.com/prometheus/alertmanager/releases/download/v0.25.0/alertmanager-0.25.0.linux-amd64.tar.gz
-mkdir alertmanager
 tar xvzf alertmanager-0.25.0.linux-amd64.tar.gz
-mv alertmanager-0.25.0* alertmanager
-adduser --system --disabled-password --shell /usr/sbin/nologin --gecos "" prometheus
+mv alertmanager-0.25.0.linux-amd64 alertmanager
+mkdir -p /opt/alertmanager/data
+/usr/sbin/adduser --system --disabled-password --shell /usr/sbin/nologin --gecos "" prometheus
 chown -R prometheus: /opt/alertmanager/
 echo '
 [Unit]
@@ -22,7 +22,8 @@ ExecStart=/opt/alertmanager/alertmanager \
 
 [Install]
 WantedBy=multi-user.target' > alertmanager.service
-# cp alertmanager.service /etc/systemd/system/
-# systemctl daemon-reload
-# systemctl enable node_exporter.service
-# systemctl start node_exporter.service
+cp alertmanager.service /etc/systemd/system/
+chown -R prometheus: /opt/alertmanager/
+systemctl daemon-reload
+systemctl enable alertmanager.service
+systemctl start alertmanager.service
