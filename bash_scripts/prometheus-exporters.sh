d4ff6e4e59ebf6075d2dc57d9fc3ec52fb783927
joshroche1 <joshua.a.roche@gmail.com>
2023-05-29 16:09:10 +0200

    Update

--- bash_scripts/prometheus-exporters.sh
+++ bash_scripts/prometheus-exporters.sh
@@ -15,5 +15,7 @@ wget https://github.com/hnlq715/nginx-vts-exporter/archive/refs/tags/v0.10.7.tar
 wget https://github.com/Nordstrom/ssh_exporter/releases/download/v2.0.0/ssh_exporter-linux-amd64
 # certificate exporter
 wget https://github.com/enix/x509-certificate-exporter/releases/download/v3.6.0-beta.2/x509-certificate-exporter-linux-amd64.tar.gz
+# snmp exporter
+wget https://github.com/prometheus/snmp_exporter/releases/download/v0.21.0/snmp_exporter-0.21.0.freebsd-amd64.tar.gz
 
 ls -al
