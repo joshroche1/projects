38dc2f7f958945d737203e8b7d39ddc7bd547882
joshroche1 <joshua.a.roche@gmail.com>
2023-05-30 21:43:07 +0200

    Update

--- bash_scripts/prometheus-node.sh
+++ bash_scripts/prometheus-node.sh
@@ -6,6 +6,11 @@ wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_e
 tar xvzf node_exporter-1.5.0.linux-amd64.tar.gz
 mv node_exporter-1.5.0.linux-amd64 node_exporter
 /usr/sbin/adduser --system --disabled-password --shell /usr/sbin/nologin --gecos "" prometheus
+echo 'tls_server_config:
+  cert_file: /opt/node_exporter/certs/server.crt
+  key_file: /opt/node_exporter/certs/server.key' > /opt/node_exporter/web.yml
+mkdir -p /opt/node_exporter/certs
+openssl req -newkey rsa:4096 -nodes -keyout /opt/node_exporter/certs/server.key -x509 -out /opt/node_exporter/certs/server.crt -subj "/C=/ST=/L=/O=/OU=/CN=/"
 chown -R prometheus: /opt/node_exporter
 echo '
 [Unit]
@@ -15,7 +20,8 @@ After=network.target
 [Service]
 User=prometheus
 Type=simple
-ExecStart=/opt/node_exporter/node_exporter
+ExecStart=/opt/node_exporter/node_exporter \
+  --web.config.file=/opt/node_exporter/web.yml
 
 [Install]
 WantedBy=multi-user.target' > node_exporter.service
